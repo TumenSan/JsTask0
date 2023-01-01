c7b42fc770c72ab494754c80cddbf4213f89fa3a
TumenSan <sandanov.tumen.sib@mail.ru>
2023-01-01 19:45:09 +0700

    com3.2

--- script1.js
+++ script1.js
@@ -1,9 +1,10 @@
 let number = 1;
 let Products = [];
 let Count = 10;
-
+//по умолчанию
 main();
 
+//количество продуктов изменено
 async function mainValue(){
     await remove();
     //Count = Number(document.getElementById("selvalue").value);
@@ -11,12 +12,14 @@ async function mainValue(){
     await main();
 }
 
+//удаляем все блоки
 function remove(){
     for(let i = 0; i < Count; i++){
         document.getElementById(i).remove();
     }
 }
 
+//Основные функции
 async function main(){
     for(let i = 1; i < Count + 1; i++){
         await GetOneProduct(i);
@@ -26,6 +29,7 @@ async function main(){
     await changeTooltip();
 }
 
+//получаем запросом один продукт
 async function GetOneProduct(number) {
     // отправляет запрос и получаем ответ
     const response = await fetch("https://dummyjson.com/products/" + number, {
@@ -42,8 +46,10 @@ async function GetOneProduct(number) {
     }
 }
 
+//добавление блоков с продуктами
 function OutDivProducts(){
     for(let i = 0; i < Count; i++){
+        //добавление блока с названием продукта
         let BLock = document.getElementsByClassName('ContainerMain')[0];
         let ObjectProduct = document.createElement('div');
         ObjectProduct.className = "ContainerBlock";
@@ -51,6 +57,7 @@ function OutDivProducts(){
         ObjectProduct.id = i;
         BLock.append(ObjectProduct);
 
+        //добавления всплывающего блока с параметрами
         let BLockDescr = document.getElementsByClassName('ContainerBlock')[i];
         let ObjectProductDescr = document.createElement('div');
         ObjectProductDescr.className = "descr";
