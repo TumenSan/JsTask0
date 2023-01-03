90bf1069034050b2cf49febdaa3202a1e4e09043
TumenSan <sandanov.tumen.sib@mail.ru>
2023-01-04 01:51:25 +0700

    com3.5

--- script1.js
+++ script1.js
@@ -18,7 +18,7 @@ async function main(){
         await GetOneProduct(i);
     }
 
-    await OutDivProducts(Count);
+    await OutDivProducts();
 }
 
 //получаем запросом один продукт
@@ -31,16 +31,15 @@ async function GetOneProduct(number) {
     // если запрос прошел нормально
     if (response.ok === true) {
         // получаем данные
-        let Product = await response.json();
-        Products.push(Product);
-        console.log(Product);
-        return Product;
+        let OneProduct = await response.json();
+        Products.push(OneProduct);
+        console.log(OneProduct);
     }
 }
 
 //добавление блоков с продуктами
-function OutDivProducts(Quantity){
-    let Count = Quantity;
+function OutDivProducts(){
+    let Count = Products.length;
     for(let i = 0; i < Count; i++){
         //добавление блока с названием продукта
         let BLock = document.getElementsByClassName('ContainerMain')[0];
@@ -62,4 +61,63 @@ function OutDivProducts(Quantity){
         ObjectProductDescr.id = 'descr_' + i;
         BLockDescr.append(ObjectProductDescr);
     }
+}
+
+
+//сортировка блоков с продуктами по Price
+function sortId(){
+  //удаляем все блоки
+  let container = document.getElementById('ContainerMain');
+  while (container.firstChild) {
+      container.removeChild(container.firstChild);
+  }
+
+  let n = Products.length;
+        
+  for(let i = 0; i < n; i++) {
+      // Находим наименьшее число в правой части массива
+      let min = i;
+      for(let j = i; j < n; j++) {
+          if(Products[j].id < Products[min].id) {
+              min=j; 
+          }
+        }
+        if (min != i) {
+            // Заменяем элементы
+            let tmp = Products[i]; 
+            Products[i] = Products[min];
+            Products[min] = tmp;      
+      }
+  }
+
+  OutDivProducts();
+}
+
+//сортировка блоков с продуктами по Price
+function sortPrice(){
+  //удаляем все блоки
+  let container = document.getElementById('ContainerMain');
+  while (container.firstChild) {
+      container.removeChild(container.firstChild);
+  }
+
+  let n = Products.length;
+        
+  for(let i = 0; i < n; i++) {
+      // Находим наименьшее число в правой части массива
+      let min = i;
+      for(let j = i; j < n; j++) {
+          if(Products[j].price < Products[min].price) {
+              min=j; 
+          }
+        }
+        if (min != i) {
+            // Заменяем элементы
+            let tmp = Products[i]; 
+            Products[i] = Products[min];
+            Products[min] = tmp;      
+      }
+  }
+
+  OutDivProducts();
 }
